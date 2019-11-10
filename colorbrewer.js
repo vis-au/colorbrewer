5ec68ad0c43bc7070d81041d6c8d95bfd544713e
mahog <mhograefer@cs.au.dk>
2019-11-10 14:06:58 +0100

    fix vega spec in text field contained outdated string

--- colorbrewer.js
+++ colorbrewer.js
@@ -291,12 +291,6 @@ function setScheme(s)
 	}
 	$("#copy-css input").val(cssString);
 
-	if (importedTemplate !== null) {
-		const compiler = new SpecCompiler();
-		const spec = compiler.getVegaSpecification(importedTemplate);
-		$("#vegaImport").val(JSON.stringify(spec, null, 2));
-	}
-
   if (selectedView !== null) {
 		const field = selectedField;
 		const type = selectedSchemeType === "sequential" ? "quantitative" : selectedSchemeType === "diverging" ? "nominal" : "ordinal";
@@ -321,6 +315,12 @@ function setScheme(s)
   }
 
 	updateVegaSpec();
+
+	if (importedTemplate !== null) {
+		const compiler = new SpecCompiler();
+		const spec = compiler.getVegaSpecification(importedTemplate);
+		$("#vegaImport").val(JSON.stringify(spec, null, 2));
+	}
 
 	$(".score-icon").attr("class","score-icon");
 	var f = checkColorblind(s);
